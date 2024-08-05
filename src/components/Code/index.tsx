648a648b95872e3dab63bcedabf9038c112e31be
Pablo Aragón <pablo.aragon@ibm.com>
2024-08-05 15:43:54 +0200

    change codeblock trheme

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import SyntaxHighlighter from 'react-syntax-highlighter'
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 interface Props {
     code: string
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export function Code({ code, theme = 'dark' }: Props) {
-    return <SyntaxHighlighter language="bash" style={dark} PreTag={"code"} CodeTag={"span"} wrapLongLines={true} customStyle={{
+    return <SyntaxHighlighter language="bash" style={atomOneDark} PreTag={"code"} CodeTag={"span"} wrapLongLines={true} customStyle={{
         display: "inline-block",
         padding: "0.125rem",
         lineHeight: "1.50rem",
